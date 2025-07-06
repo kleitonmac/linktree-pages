cea557e02ef750db7503fb1f1d3746fb937f6fef
kleitonmac <kleiton76@yahoo.com>
2025-07-06 13:36:00 -0300

    Pagess

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,7 +1,9 @@
 import CustomLink from "./CustomLink";
 import {IconBrandInstagram, 
         IconBrandWhatsapp, 
-        IconBrandFacebook, 
+        IconBrandFacebook,
+        IconBrandGithub,
+        IconBrandLinkedin
 }
 from "@tabler/icons-react";
 
@@ -12,6 +14,8 @@ export default function NavBar() {
   <CustomLink href="https://www.instagram.com/" icon={<IconBrandInstagram size={42} color="#ffffff" />} />
   <CustomLink href="https://wa.me/" icon={<IconBrandWhatsapp size={42} color="#ffffff" />} />
   <CustomLink href="https://www.facebook.com/" icon={<IconBrandFacebook size={42} color="#ffffff" />} />
+  <CustomLink href="https://github.com/" icon={<IconBrandGithub size={42} color="#ffffff" />} />
+  <CustomLink href="https://www.linkedin.com/" icon={<IconBrandLinkedin size={42} color="#ffffff" />} />
         </div>
 
            <ul>
@@ -19,6 +23,8 @@ export default function NavBar() {
                      <CustomLink href="https://www.instagram.com/iamkleiton/">Instagram</CustomLink>
                      <CustomLink href="https://www.facebook.com/kleiton.santosmacedo">Facebook</CustomLink>
                      <CustomLink href="https://wa.me/5527981911375">WhatsApp</CustomLink>
+                     <CustomLink href="https://github.com/kleitonmac">GitHub</CustomLink>
+                     <CustomLink href="https://www.linkedin.com/in/kleitonmacedo/">Linkedin</CustomLink>
                </div>
            </ul>
     </nav>
